a824ff251717aa8e52c336fd53dac159cd15bd87
Anna Dunajeva <anna.dunajeva@gmail.com>
2020-06-02 13:46:05 +0300

    config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'docs'),
         filename: 'index_bundle.js',
-        publicPath: '/'
+        publicPath: process.env.NODE_ENV === 'production' ? '/hacker-news-clone/' :'/'
     },
     module: {
         rules: [
