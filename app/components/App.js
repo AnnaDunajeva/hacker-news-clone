7200c28bb0403d9224019c3c9e9d03fe321f669e
Anna Dunajeva <anna.dunajeva@gmail.com>
2020-06-02 15:40:55 +0300

    build

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 import Nav from './Nav.js'
 import {ThemeContext} from '../context/ThemeContext.js'
 import Top from './Top.js'
